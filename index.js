acc914cdf614673be09919027f46cefb58e6e239
unknown <khedkarmahesh14@gmail.com>
2023-09-21 11:40:50 +0530

    hover effect added to navbar

--- index.js
+++ index.js
@@ -21,6 +21,85 @@ function toggleMenu()
     }
 }
 
+
+//hover list divs
+
+        function hoverDiv1()
+        {
+            var listDiv1 = document.querySelector('.listDiv1');
+
+            if (listDiv1.style.display === 'none' || listDiv1.style.display === '' ) 
+            {
+                listDiv1.style.display = 'flex'; // Display the menu
+            }
+            else 
+            {
+                listDiv1.style.display = 'none'; // Hide the menu
+            }
+        }
+
+        let hoverBTN1 = document.querySelector('.hoverBTN1');
+        hoverBTN1.addEventListener('mouseover', hoverDiv1);
+
+        function hoverDiv11()
+        {
+            var listDiv1 = document.querySelector('.listDiv1');
+
+            if (listDiv1.style.display === 'flex' || listDiv1.style.display === '' ) 
+            {
+                listDiv1.style.display = 'none'; // Display the menu
+            }
+            else 
+            {
+                listDiv1.style.display = 'flex'; // Hide the menu
+            }
+        }
+
+        // let hoverBTN1 = document.querySelector('.hoverBTN1');
+
+        hoverBTN1.addEventListener('mouseout', hoverDiv11);
+
+
+        function hoverDiv2()
+        {
+            var listDiv2 = document.querySelector('.listDiv2');
+
+
+            if (listDiv2.style.display === 'none' || listDiv2.style.display === '' ) 
+            {
+                listDiv2.style.display = 'flex'; // Display the menu close
+            }
+            else 
+            {
+                listDiv2.style.display = 'none'; // Display the menu
+            }
+        }
+
+        let hoverBTN2 = document.querySelector('.hoverBTN2');
+
+        hoverBTN2.addEventListener('mouseover', hoverDiv2);
+
+
+        function hoverDiv22()
+        {
+            var listDiv2 = document.querySelector('.listDiv2');
+
+
+            if (listDiv2.style.display === 'flex' || listDiv2.style.display === '' ) 
+            {
+                listDiv2.style.display = 'none'; // Display the menu close
+            }
+            else 
+            {
+                listDiv2.style.display = 'flex'; // Display the menu
+            }
+        }
+
+        // let hoverBTN2 = document.querySelector('.hoverBTN2');
+
+        hoverBTN2.addEventListener('mouseout', hoverDiv22);
+
+
 //-----------Slider code-------------------------------//
 
 const slides = document.querySelectorAll('.slide');
